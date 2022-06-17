8263ad52fa9507f54bc41672adbb34c686676fae
150pocahontas <patricia@DESKTOP-5MJ5UL3.localdomain>
2022-06-17 22:31:52 +0100

    allValidPlays

--- Adventurers.hs
+++ Adventurers.hs
@@ -9,12 +9,12 @@ data Adventurer = P1 | P2 | P5 | P10 deriving (Show,Eq)
 type Objects = Either Adventurer ()
 
 -- The time that each adventurer needs to cross the bridge
--- To implement 
+-- To implement
 getTimeAdv :: Adventurer -> Int
 getTimeAdv P1 = 1
 getTimeAdv P2 = 2
-getTimeAdv P3 = 5
-getTimeAdv P4 = 10
+getTimeAdv P5 = 5
+getTimeAdv P10 = 10
 
 {-- The state of the game, i.e. the current position of each adventurer
 + the lantern. The function (const False) represents the initial state
@@ -48,43 +48,45 @@ gInit = const False
 changeState :: Objects -> State -> State
 changeState a s = let v = s a in (\x -> if x == a then not v else s x)
 
--- Changes the state of the game of a list of objects 
+-- Changes the state of the game of a list of objects
 mChangeState :: [Objects] -> State -> State
 mChangeState os s = foldr changeState s os
-                               
-----------------------------------AUXILIARES X-----------------------------------
--- Returns the list of adventurers who can cross to the opposite side of the lantern 
-whoCanCross :: State -> [Objects]
-whoCanCross s = filter (\x -> (s x) == (s (Right ()))) [Left P1, Left P2, Left P5, Left P10]
 
--- Returns all combinations with one or two elements of the list
-allComb :: [a] -> [[a]]
-allComb [] = []
-allComb (x:xs) = [[x]] ++ map (\y -> [x,y]) xs ++ allComb xs
+-- Filter the adventures in the same side as the lantern
+advWLantern :: State -> [Objects]
+advWLantern s = filter (\a -> (s a) == (s (Right ()))) [Left P1, Left P2, Left P5, Left P10]
+
+-- Combine all adventurers on the same side
+advComb :: [a] -> [[a]]
+advComb [] = []
+advComb (x:xs) = [[x]] ++ map (\y -> [x,y]) xs ++ advComb xs
+
+-- Get the Adventure from Objects
+getAdventurer :: [Objects] -> [Adventurer]
+getAdventurer [] = []
+getAdventurer ((Left x):xs) = [x] ++ (getAdventurer xs)
+getAdventurer ((Right ()):xs) = getAdventurer xs
+
+-- The time that a group of adventurers needs to cross the bridge
+crossingT :: [Objects] -> Int
+crossingT = maximum . map getTimeAdv . getAdventurer
 
 {-- For a given state of the game, the function presents all changes
-that can be applied to the state together with the time that adventurers 
+that can be applied to the state together with the time that adventurers
 take to cross the bridge in that change. --}
-allChangeState :: State -> ListDur (State -> State)
-allChangeState s = LD $ map (\l -> Duration(time2cross l, mChangeState (l ++ [Right()]))) (allComb . whoCanCross $ s)
-
-
----------------------------------------------------------------------------------------
-
+changeAllStates :: State -> ListDur State
+changeAllStates = LD . map (\l -> Duration(crossingT l, mChangeState (l ++ [Right()]))) . advComb . advWLantern
 
 {-- For a given state of the game, the function presents all the
 possible moves that the adventurers can make.  --}
--- To implement
 allValidPlays :: State -> ListDur State
-allValidPlays s = (allChangeState s) <*> (return s)
-
+allValidPlays = changeAllStates <*> return
 
 {-- For a given number n and initial state, the function calculates
 all possible n-sequences of moves that the adventures can make --}
--- To implement 
 exec :: Int -> State -> ListDur State
 exec 0 s = return s
-exec n s = let l = exec (n-1) s in 
+exec n s = let l = exec (n-1) s in
    l >>= allValidPlays
 
 
@@ -92,8 +94,8 @@ exec n s = let l = exec (n-1) s in
 check :: Int -> Int -> ListDur State -> Bool
 check 0 t l = False
 check n t (LD []) = False
-check n t l = let l' = remLD $ l >>= allValidPlays in 
-   if any (\x -> (getDuration x <= t) && (getValue x == gEnd)) l'
+check n t l = let l' = remLD $ l >>= allValidPlays in
+   if any (\x -> (getDuration x <= t) && (getValue x == (const True))) l'
       then True
       else check (n-1) t $ LD $ filter (\x -> (getDuration x <= t)) l'
 
@@ -111,8 +113,8 @@ leq17 = check 5 17 $ return gInit
 
 check2 :: Int -> ListDur State -> Bool
 check2 t (LD []) = False
-check2 t l = let l' = remLD $ l >>= allValidPlays in 
-   if any (\x -> (getDuration x < t) && (getValue x == gEnd)) l'
+check2 t l = let l' = remLD $ l >>= allValidPlays in
+   if any (\x -> (getDuration x < t) && (getValue x == (const True))) l'
       then True
       else check2 t $ LD $ filter (\x -> (getDuration x < t)) l'
 
@@ -126,33 +128,29 @@ l17 = check2 17 $ return gInit
 
 
 --------------------------------------------------------------------------
-{-- Implementation of the monad used for the problem of the adventurers.
-Recall the Knight's quest --}
+-- Implementation of the monad used for the problem of the adventurers.
 
 data ListDur a = LD [Duration a] deriving Show
 
 remLD :: ListDur a -> [Duration a]
 remLD (LD x) = x
 
--- To implement
 instance Functor ListDur where
-   fmap f = let f' = fmap f in
-      LD . (map f') . remLD
+  fmap f = let f' = fmap f in
+    LD . (map f') . remLD
 
--- To implement
 instance Applicative ListDur where
-   pure x = LD [pure x]
-   l1 <*> l2 = LD $ do x <- remLD l1
-                       y <- remLD l2
-                       g(x,y) where
-                         g(x,y) = return $ x <*> y
+  pure x = LD [pure x]
+  l1 <*> l2 = LD $ do x <- remLD l1
+                      y <- remLD l2
+                      g(x,y) where
+                        g(x,y) = return $ x <*> y
 
--- To implement
 instance Monad ListDur where
-   return = pure
-   l >>= k = LD $ do x <- remLD l
-                     g x where
-                        g(Duration(a,b)) = let u = (remLD (k b)) in map (wait a) u
+  return = pure
+  l >>= k = LD $ do x <- remLD l
+                    g x where
+                      g(Duration(d,x)) = let u = (remLD (k x)) in map (wait d) u
 
 manyChoice :: [ListDur a] -> ListDur a
 manyChoice = LD . concat . (map remLD)
